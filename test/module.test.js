3e1271e38af27e7340592d4b0706090e8364c28c
Stephen Kerr <stephenkerr0312@gmail.com>
2019-05-20 21:18:22 +0200

    test: Add some spec coverage

--- test/module.test.js
+++ test/module.test.js
@@ -48,11 +48,99 @@ describe('FeatureToggle', () => {
 
       expect(actual).toBe(false)
     })
+
+    it('should return true if the query string matches and is enabled', () => {
+      const wrapper = getWrapper({
+        extraQuery: {
+          'toggle_my-unique-toggle': 'true'
+        },
+        extraFeatureToggle: {
+          queryString: true
+        }
+      })
+
+      const actual = wrapper.vm.canShow
+
+      expect(actual).toBe(true)
+    })
   })
 
   describe('canShowWithQueryString', () => {
     it('should return true if the query string matches', () => {
+      const wrapper = getWrapper({
+        extraQuery: {
+          'toggle_my-unique-toggle': 'true'
+        },
+        extraFeatureToggle: {
+          queryString: true
+        }
+      })
 
+      const actual = wrapper.vm.canShowWithQueryString
+
+      expect(actual).toBe(true)
+    })
+  })
+
+  describe('queryStringKey', () => {
+    it('should return the correct key', () => {
+      const wrapper = getWrapper()
+
+      const actual = wrapper.vm.queryStringKey
+
+      expect(actual).toBe('toggle_my-unique-toggle')
+    })
+  })
+
+  describe('queryString', () => {
+    it('should return the true if the query string is enabled', () => {
+      const wrapper = getWrapper({
+        extraFeatureToggle: {
+          queryString: true
+        }
+      })
+      const actual = wrapper.vm.queryString
+
+      expect(actual).toBe(true)
+    })
+
+    it('should return the false if the query string is disabled', () => {
+      const wrapper = getWrapper({
+        extraFeatureToggle: {
+          queryString: false
+        }
+      })
+      const actual = wrapper.vm.queryString
+
+      expect(actual).toBe(false)
+    })
+  })
+
+  describe('isQueryStringAllowed', () => {
+    it('should return true if is allowed', () => {
+      const wrapper = getWrapper({
+        extraFeatureToggle: {
+          queryString: true,
+          isQueryStringAllowedFn: () => true
+        }
+      })
+
+      const actual = wrapper.vm.isQueryStringAllowed
+
+      expect(actual).toBe(true)
+    })
+
+    it('should return false if not allowed', () => {
+      const wrapper = getWrapper({
+        extraFeatureToggle: {
+          queryString: true,
+          isQueryStringAllowedFn: () => false
+        }
+      })
+
+      const actual = wrapper.vm.isQueryStringAllowed
+
+      expect(actual).toBe(false)
     })
   })
 })
